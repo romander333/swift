68530dad768eb2fcbcbe75399cf55837c084933d
ІПЗ-31/2 <ipz-312@Administrators-iMac-6.local>
2025-09-04 01:30:03 -0700

    changed text

--- first_project/ContentView.swift
+++ first_project/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("My first project")
         }
         .padding()
     }
