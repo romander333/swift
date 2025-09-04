3d7af7b99d1881984b047268ae10aa91bbf09e5b
ІПЗ-31/2 <ipz-312@Administrators-iMac-6.local>
2025-09-04 01:19:53 -0700

    ///

---
+++ first_project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  first_project
+//
+//  Created by ІПЗ-31/2 on 03.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ first_project/first_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_projectApp.swift
+//  first_project
+//
+//  Created by ІПЗ-31/2 on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct first_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
